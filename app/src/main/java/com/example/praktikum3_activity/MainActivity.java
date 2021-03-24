bd4738dd0e2da7bf1bbe568ab5e74f4d78c3988d
Aulia-Dinin-Putri <65695280+Aulia-Dinin-Putri@users.noreply.github.com>
2021-03-24 13:00:44 +0000

    Praktikum Pertama

---
+++ app/src/main/java/com/example/praktikum3_activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.praktikum3_activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
